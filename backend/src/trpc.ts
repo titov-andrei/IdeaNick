93c11faa5cd5902af86533b2daab938302bca419
Andrew E. Titov <titov_andrei@mail.ru>
2025-02-17 06:08:58 +0300

    query react test

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -12,6 +12,7 @@ const trpc = initTRPC.create()
 
 export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
+    // throw new Error("Test error")
     return { ideas }
   }),
 })
--- webapp/src/pages/AllIdeasPage/index.tsx
+++ webapp/src/pages/AllIdeasPage/index.tsx
@@ -1,18 +1,27 @@
 import { trpc } from '../../lib/trpc'
 
 export const AllIdeasPage = () => {
-  const result = trpc.getIdeas.useQuery()
-  console.log(result)
-  return null
-  // return (
-  //   <div>
-  //     <h1>Ideanick</h1>
-  //     {ideas.map((idea) => (
-  //       <div key={idea.nick}>
-  //         <h2>{idea.name}</h2>
-  //         <p>{idea.description}</p>
-  //       </div>
-  //     ))}
-  //   </div>
-  // )
+  const { data, error, isLoading, isFetching, isError } = trpc.getIdeas.useQuery()
+
+  if (isLoading || isFetching) {
+    return <span>Loading...</span>
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>
+  }
+  // const result = trpc.getIdeas.useQuery()
+  // console.log(result)
+  // return null
+  return (
+    <div>
+      <h1>All Ideas</h1>
+      {data.ideas.map((idea) => (
+        <div key={idea.nick}>
+          <h2>{idea.name}</h2>
+          <p>{idea.description}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
