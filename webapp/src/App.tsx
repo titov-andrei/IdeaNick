dc63dd0e670eb9a4e756db41fc22113b2ac7cc90
Andrew E. Titov <titov_andrei@mail.ru>
2025-02-21 11:15:48 +0300

    add monorepo scripts

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -7,6 +7,6 @@ export const App = () => {
   return (
     <TrpcProvider>
       <AllIdeasPage />
-    </TrpcProvider>  
+    </TrpcProvider>
   )
 }
--- webapp/src/lib/trpc.tsx
+++ webapp/src/lib/trpc.tsx
@@ -6,26 +6,26 @@ import { httpBatchLink } from '@trpc/client'
 export const trpc = createTRPCReact<TrpcRouter>()
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            retry: false,
-            refetchOnWindowFocus: false,
-        },
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
     },
+  },
 })
 
 const trpcClient = trpc.createClient({
-    links: [
-        httpBatchLink({
-            url: 'http://localhost:3000/trpc',
-        }),
-    ],
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:3000/trpc',
+    }),
+  ],
 })
 
 export const TrpcProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
-        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </trpc.Provider>        
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
   )
 }
