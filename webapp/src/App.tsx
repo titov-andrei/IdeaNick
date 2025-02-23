a837ddb28072a0e3fbba9073bf63641342d07a19
Andrew E. Titov <titov_andrei@mail.ru>
2025-02-23 17:21:34 +0300

    Add routes and link

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,12 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { getAllIdeasRoute, getViewIdeaRoute } from './lib/routes'
 import { TrpcProvider } from './lib/trpc'
 import { AllIdeasPage } from './pages/AllIdeasPage'
-
-// const x: string = 1
+import { ViewIdeaPage } from './pages/ViewIdeaPage'
 
 export const App = () => {
   return (
     <TrpcProvider>
-      <AllIdeasPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
+          <Route path={getViewIdeaRoute({ ideaNick: ':ideaNick' })} element={<ViewIdeaPage />} />
+        </Routes>
+      </BrowserRouter>
     </TrpcProvider>
   )
 }
