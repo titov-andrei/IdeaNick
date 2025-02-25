e4325ee8f258ade82f25bd0c51092378680d0f96
Andrew E. Titov <titov_andrei@mail.ru>
2025-02-25 10:36:19 +0300

    Add zod

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,5 +1,6 @@
 import { initTRPC } from '@trpc/server'
 import _ from 'lodash'
+import { z } from 'zod'
 
 const ideas = _.times(100, (i) => ({
   nick: `cool-idea-nick-${i}`,
@@ -8,14 +9,6 @@ const ideas = _.times(100, (i) => ({
   text: _.times(100, (j) => `<p>Text paragraph ${j} of idea ${i}...</p>`).join(''),
 }))
 
-// const ideas = [
-//   { nick: 'cool-idea-nick-1', name: 'Idea 1', description: 'Description of idea 1...' },
-//   { nick: 'cool-idea-nick-2', name: 'Idea 2', description: 'Description of idea 2...' },
-//   { nick: 'cool-idea-nick-3', name: 'Idea 3', description: 'Description of idea 3...' },
-//   { nick: 'cool-idea-nick-4', name: 'Idea 4', description: 'Description of idea 4...' },
-//   { nick: 'cool-idea-nick-5', name: 'Idea 5', description: 'Description of idea 5...' },
-// ]
-
 const trpc = initTRPC.create()
 
 export const trpcRouter = trpc.router({
@@ -23,6 +16,16 @@ export const trpcRouter = trpc.router({
     // throw new Error("Test error")
     return { ideas: ideas.map((idea) => _.pick(idea, ['nick', 'name', 'description'])) }
   }),
+  getIdea: trpc.procedure
+    .input(
+      z.object({
+        ideaNick: z.string(),
+      })
+    )
+    .query(({ input }) => {
+      const idea = ideas.find((idea) => idea.nick === input.ideaNick)
+      return { idea: idea || null }
+    }),
 })
 
 export type TrpcRouter = typeof trpcRouter
