ee986f9bfdc82698e3636010ffe82dd5a14367c4
Andrew E. Titov <titov_andrei@mail.ru>
2025-03-06 11:00:08 +0300

    Add Segment in code for re-use

--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom'
+import { Segment } from '../../components/Segment'
 import { type ViewIdeaRouteParams } from '../../lib/routes'
 import { trpc } from '../../lib/trpc'
 import css from './index.module.scss'
@@ -23,10 +24,8 @@ export const ViewIdeaPage = () => {
   }
 
   return (
-    <div>
-      <h1 className={css.title}>{data.idea.name}</h1>
-      <p className={css.description}>{data.idea.description}</p>
+    <Segment title={data.idea.name} description={data.idea.description}>
       <div className={css.text} dangerouslySetInnerHTML={{ __html: data.idea.text }} />
-    </div>
+    </Segment>
   )
 }
