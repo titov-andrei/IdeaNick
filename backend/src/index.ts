89ee6f1c2710cb70549a05bac0f0bfee27c019f6
Andrew E. Titov <titov_andrei@mail.ru>
2025-03-12 08:16:26 +0300

    Change files structure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,14 @@
-import express from 'express'
-import * as trpcExpress from '@trpc/server/adapters/express'
-import { trpcRouter } from './trpc'
 import cors from 'cors'
+import express from 'express'
+import { applyTrpcToExpressApp } from './lib/trpc'
+import { trpcRouter } from './router'
 
 const expressApp = express()
 expressApp.use(cors())
 expressApp.get('/ping', (req, res) => {
   res.send('pong')
 })
-expressApp.use(
-  '/trpc',
-  trpcExpress.createExpressMiddleware({
-    router: trpcRouter,
-  })
-)
+applyTrpcToExpressApp(expressApp, trpcRouter)
 expressApp.listen(3000, () => {
   console.info('Listening at http://localhost:3000')
 })
