3e8cd8ac4117d8ac7e8e1712d49cf3ebfc45cbc9
Andrew E. Titov <titov_andrei@mail.ru>
2025-03-15 10:01:48 +0300

    Add mutation for createIdea

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -4,8 +4,10 @@ import { z } from 'zod'
 import { Input } from '../../components/Input'
 import { Segment } from '../../components/Segment'
 import { Textarea } from '../../components/Textarea'
+import { trpc } from '../../lib/trpc'
 
 export const NewIdeaPage = () => {
+  const createIdea = trpc.createIdea.useMutation()
   const formik = useFormik({
     initialValues: {
       name: '',
@@ -24,8 +26,8 @@ export const NewIdeaPage = () => {
         text: z.string().min(100, 'Text should be at least 100 characters long'),
       })
     ),
-    onSubmit: (values) => {
-      console.info('Submitted', values)
+    onSubmit: async (values) => {
+      await createIdea.mutateAsync(values)
     },
   })
 
