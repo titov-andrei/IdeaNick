c67575f79d62c7156765749eea7bd17b9266258f
Andrew E. Titov <titov_andrei@mail.ru>
2025-03-29 11:15:30 +0300

    Edit trpc

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,24 @@
 import cors from 'cors'
 import express from 'express'
+import { type AppContext, createAppContext } from './lib/ctx'
 import { applyTrpcToExpressApp } from './lib/trpc'
 import { trpcRouter } from './router'
 
-const expressApp = express()
-expressApp.use(cors())
-expressApp.get('/ping', (req, res) => {
-  res.send('pong')
-})
-applyTrpcToExpressApp(expressApp, trpcRouter)
-expressApp.listen(3000, () => {
-  console.info('Listening at http://localhost:3000')
-})
+void (async () => {
+  let ctx: AppContext | null = null
+  try {
+    ctx = createAppContext()
+    const expressApp = express()
+    expressApp.use(cors())
+    expressApp.get('/ping', (req, res) => {
+      res.send('pong')
+    })
+    applyTrpcToExpressApp(expressApp, ctx, trpcRouter)
+    expressApp.listen(3000, () => {
+      console.info('Listening at http://localhost:3000')
+    })
+  } catch (error) {
+    console.error(error)
+    await ctx?.stop()
+  }
+})()
