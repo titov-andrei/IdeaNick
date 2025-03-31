87a5a2b6ff295b43926f190088f78a996a0112a2
Andrew E. Titov <titov_andrei@mail.ru>
2025-03-31 13:35:47 +0300

    Edit time format out

--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -1,3 +1,4 @@
+import { format } from 'date-fns/format'
 import { useParams } from 'react-router-dom'
 import { Segment } from '../../components/Segment'
 import { type ViewIdeaRouteParams } from '../../lib/routes'
@@ -25,6 +26,7 @@ export const ViewIdeaPage = () => {
 
   return (
     <Segment title={data.idea.name} description={data.idea.description}>
+      <div className={css.createdAt}>Created At: {format(data.idea.createdAt, 'yyyy-MM-dd')}</div>
       <div className={css.text} dangerouslySetInnerHTML={{ __html: data.idea.text }} />
     </Segment>
   )
