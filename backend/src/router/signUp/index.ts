d0d32498688d4f435f1590d3764ca5d525975f1e
Andrew E. Titov <titov_andrei@mail.ru>
2025-04-01 08:15:42 +0300

    Add signUp on backend

---
+++ backend/src/router/signUp/index.ts
@@ -0,0 +1,21 @@
+import crypto from 'crypto'
+import { trpc } from '../../lib/trpc'
+import { zSignUpTrpcInput } from './input'
+
+export const signUpTrpcRoute = trpc.procedure.input(zSignUpTrpcInput).mutation(async ({ ctx, input }) => {
+  const exUser = await ctx.prisma.user.findUnique({
+    where: {
+      nick: input.nick,
+    },
+  })
+  if (exUser) {
+    throw new Error('User with this nick already exists')
+  }
+  await ctx.prisma.user.create({
+    data: {
+      nick: input.nick,
+      password: crypto.createHash('sha256').update(input.password).digest('hex'),
+    },
+  })
+  return true
+})
