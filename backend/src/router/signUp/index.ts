e1569a98ccaec0d3e282d9849b48198c7adb63b6
Andrew E. Titov <titov_andrei@mail.ru>
2025-04-12 11:16:53 +0300

    Add signIn

--- backend/src/router/signUp/index.ts
+++ backend/src/router/signUp/index.ts
@@ -1,5 +1,5 @@
-import crypto from 'crypto'
 import { trpc } from '../../lib/trpc'
+import { getPasswordHash } from '../../utils/getPasswordHash'
 import { zSignUpTrpcInput } from './input'
 
 export const signUpTrpcRoute = trpc.procedure.input(zSignUpTrpcInput).mutation(async ({ ctx, input }) => {
@@ -14,7 +14,7 @@ export const signUpTrpcRoute = trpc.procedure.input(zSignUpTrpcInput).mutation(a
   await ctx.prisma.user.create({
     data: {
       nick: input.nick,
-      password: crypto.createHash('sha256').update(input.password).digest('hex'),
+      password: getPasswordHash(input.password),
     },
   })
   return true
