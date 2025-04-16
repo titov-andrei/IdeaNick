58b9d3237cbcc6c08fe58a2543cd9755b8554430
Andrew E. Titov <titov_andrei@mail.ru>
2025-04-16 12:14:48 +0300

    Add SignInPage

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -4,6 +4,7 @@ import * as routes from './lib/routes'
 import { TrpcProvider } from './lib/trpc'
 import { AllIdeasPage } from './pages/AllIdeasPage'
 import { NewIdeaPage } from './pages/NewIdeaPage'
+import { SignInPage } from './pages/SignInPage'
 import { SignUpPage } from './pages/SignUpPage'
 import { ViewIdeaPage } from './pages/ViewIdeaPage'
 import './styles/global.scss'
@@ -15,6 +16,7 @@ export const App = () => {
         <Routes>
           <Route element={<Layout />}>
             <Route path={routes.getSignUpRoute()} element={<SignUpPage />} />
+            <Route path={routes.getSignInRoute()} element={<SignInPage />} />
             <Route path={routes.getAllIdeasRoute()} element={<AllIdeasPage />} />
             <Route path={routes.getNewIdeaRoute()} element={<NewIdeaPage />} />
             <Route path={routes.getViewIdeaRoute(routes.viewIdeaRouteParams)} element={<ViewIdeaPage />} />
