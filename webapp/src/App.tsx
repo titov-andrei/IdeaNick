19f5c056eb9a1460f133de3f8a9040998f4a8a2d
Andrew E. Titov <titov_andrei@mail.ru>
2025-07-20 21:07:25 +0300

    Edit autorization files and create OutPage

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -5,6 +5,7 @@ import { TrpcProvider } from './lib/trpc'
 import { AllIdeasPage } from './pages/AllIdeasPage'
 import { NewIdeaPage } from './pages/NewIdeaPage'
 import { SignInPage } from './pages/SignInPage'
+import { SignOutPage } from './pages/SignOutPage'
 import { SignUpPage } from './pages/SignUpPage'
 import { ViewIdeaPage } from './pages/ViewIdeaPage'
 import './styles/global.scss'
@@ -14,6 +15,7 @@ export const App = () => {
     <TrpcProvider>
       <BrowserRouter>
         <Routes>
+          <Route path={routes.getSignOutRoute()} element={<SignOutPage />} />
           <Route element={<Layout />}>
             <Route path={routes.getSignUpRoute()} element={<SignUpPage />} />
             <Route path={routes.getSignInRoute()} element={<SignInPage />} />
--- webapp/src/components/Layout/index.tsx
+++ webapp/src/components/Layout/index.tsx
@@ -1,8 +1,11 @@
 import { Link, Outlet } from 'react-router-dom'
-import { getAllIdeasRoute, getNewIdeaRoute, getSignInRoute, getSignUpRoute } from '../../lib/routes'
+import { getAllIdeasRoute, getNewIdeaRoute, getSignInRoute, getSignOutRoute, getSignUpRoute } from '../../lib/routes'
+import { trpc } from '../../lib/trpc'
 import css from './index.module.scss'
 
 export const Layout = () => {
+  const { data, isLoading, isFetching, isError } = trpc.getMe.useQuery()
+
   return (
     <div className={css.layout}>
       <div className={css.navigation}>
@@ -13,21 +16,33 @@ export const Layout = () => {
               All Ideas
             </Link>
           </li>
-          <li className={css.item}>
-            <Link className={css.link} to={getNewIdeaRoute()}>
-              Add Idea
-            </Link>
-          </li>
-          <li className={css.item}>
-            <Link className={css.link} to={getSignUpRoute()}>
-              Sign Up
-            </Link>
-          </li>
-          <li className={css.item}>
-            <Link className={css.link} to={getSignInRoute()}>
-              Sign In
-            </Link>
-          </li>
+          {isLoading || isFetching || isError ? null : data.me ? (
+            <>
+              <li className={css.item}>
+                <Link className={css.link} to={getNewIdeaRoute()}>
+                  Add Idea
+                </Link>
+              </li>
+              <li className={css.item}>
+                <Link className={css.link} to={getSignOutRoute()}>
+                  Log Out ({data.me.nick})
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className={css.item}>
+                <Link className={css.link} to={getSignUpRoute()}>
+                  Sign Up
+                </Link>
+              </li>
+              <li className={css.item}>
+                <Link className={css.link} to={getSignInRoute()}>
+                  Sign In
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
       <div className={css.content}>
