1c1a4ae355ba23a4c4508c3ea779eeba8448f850
Andrew E. Titov <titov_andrei@mail.ru>
2025-07-25 06:46:26 +0300

    Edit env config

---
+++ backend/src/lib/env.ts
@@ -0,0 +1,12 @@
+import * as dotenv from 'dotenv'
+import { z } from 'zod'
+
+dotenv.config()
+
+const zEnv = z.object({
+  PORT: z.string().trim().min(1),
+  DATABASE_URL: z.string().trim().min(1),
+  JWT_SECRET: z.string().trim().min(1),
+})
+
+export const env = zEnv.parse(process.env)
