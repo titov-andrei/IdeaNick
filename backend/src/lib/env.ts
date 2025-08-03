bfcfbc03b85c8fa4784f2f3faa35237ab55bb375
Andrew E. Titov <titov_andrei@mail.ru>
2025-08-03 13:28:38 +0300

    Add pass-salt

--- backend/src/lib/env.ts
+++ backend/src/lib/env.ts
@@ -7,6 +7,8 @@ const zEnv = z.object({
   PORT: z.string().trim().min(1),
   DATABASE_URL: z.string().trim().min(1),
   JWT_SECRET: z.string().trim().min(1),
+  PASSWORD_SALT: z.string().trim().min(1),
 })
 
+/*// eslint-disable-next-line node/no-process-env*/
 export const env = zEnv.parse(process.env)
