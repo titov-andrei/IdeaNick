239786393df463647f5d6127279e9bb30fcf167a
Andrew E. Titov <titov_andrei@mail.ru>
2025-08-24 08:52:14 +0300

    Add edit page

--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -1,34 +1,48 @@
 import { format } from 'date-fns/format'
 import { useParams } from 'react-router-dom'
+import { LinkButton } from '../../components/Button'
 import { Segment } from '../../components/Segment'
-import { type ViewIdeaRouteParams } from '../../lib/routes'
+import { getEditIdeaRoute, type ViewIdeaRouteParams } from '../../lib/routes'
 import { trpc } from '../../lib/trpc'
 import css from './index.module.scss'
 
 export const ViewIdeaPage = () => {
   const { ideaNick } = useParams() as ViewIdeaRouteParams
 
-  const { data, error, isLoading, isFetching, isError } = trpc.getIdea.useQuery({
+  const getIdeaResult = trpc.getIdea.useQuery({
     ideaNick,
   })
+  const getMeResult = trpc.getMe.useQuery()
 
-  if (isLoading || isFetching) {
+  if (getIdeaResult.isLoading || getIdeaResult.isFetching || getMeResult.isLoading || getMeResult.isFetching) {
     return <span>Loading...</span>
   }
 
-  if (isError) {
-    return <span>Error: {error.message}</span>
+  if (getIdeaResult.isError) {
+    return <span>Error: {getIdeaResult.error.message}</span>
   }
 
-  if (!data.idea) {
+  if (getMeResult.isError) {
+    return <span>Error: {getMeResult.error.message}</span>
+  }
+
+  if (!getIdeaResult.data.idea) {
     return <span>Idea not found</span>
   }
 
+  const idea = getIdeaResult.data.idea
+  const me = getMeResult.data.me
+
   return (
-    <Segment title={data.idea.name} description={data.idea.description}>
-      <div className={css.createdAt}>Created At: {format(data.idea.createdAt, 'yyyy-MM-dd')}</div>
-      <div className={css.author}>Author: {data.idea.author.nick}</div>
-      <div className={css.text} dangerouslySetInnerHTML={{ __html: data.idea.text }} />
+    <Segment title={idea.name} description={idea.description}>
+      <div className={css.createdAt}>Created At: {format(idea.createdAt, 'yyyy-MM-dd')}</div>
+      <div className={css.author}>Author: {idea.author.nick}</div>
+      <div className={css.text} dangerouslySetInnerHTML={{ __html: idea.text }} />
+      {me?.id === idea.authorId && (
+        <div className={css.editButton}>
+          <LinkButton to={getEditIdeaRoute({ ideaNick: idea.nick })}>Edit Idea</LinkButton>
+        </div>
+      )}
     </Segment>
   )
 }
