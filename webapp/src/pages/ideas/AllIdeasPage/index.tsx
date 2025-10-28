dfd78feab432527dfb52893f3cb3f4b417da1a00
Andrew E. Titov <titov_andrei@mail.ru>
2025-10-28 10:55:29 +0300

    Add loader

--- webapp/src/pages/ideas/AllIdeasPage/index.tsx
+++ webapp/src/pages/ideas/AllIdeasPage/index.tsx
@@ -2,6 +2,7 @@ import InfiniteScroll from 'react-infinite-scroller'
 import { Link } from 'react-router-dom'
 import { Alert } from '../../../components/Alert'
 import { layoutContentElRef } from '../../../components/Layout'
+import { Loader } from '../../../components/Loader'
 import { Segment } from '../../../components/Segment'
 import { getViewIdeaRoute } from '../../../lib/routes'
 import { trpc } from '../../../lib/trpc'
@@ -21,7 +22,7 @@ export const AllIdeasPage = () => {
   return (
     <Segment title="All Ideas">
       {isLoading || isRefetching ? (
-        <div>Loading...</div>
+        <Loader type="section" />
       ) : isError ? (
         <Alert color="red">{error.message}</Alert>
       ) : (
@@ -36,7 +37,7 @@ export const AllIdeasPage = () => {
             hasMore={hasNextPage}
             loader={
               <div className={css.more} key="loader">
-                Loading...
+                <Loader type="section" />
               </div>
             }
             getScrollParent={() => layoutContentElRef.current}
